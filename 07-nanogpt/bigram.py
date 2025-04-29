f30ebf010d85aec010c7e9ebef69dbc1575628e9
Matthew Danbury <46234082+mjdanbury@users.noreply.github.com>
2025-04-28 23:12:49 -0400

    scale and train

--- 07-nanogpt/bigram.py
+++ 07-nanogpt/bigram.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 #hyperparameters
-batch_size = 32 #how many independent sequences will we process in parallel?
-block_size = 8 #what is the maximum context length for predictions?
+batch_size = 64 #how many independent sequences will we process in parallel?
+block_size = 256 #what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embed = 32
+n_embed = 384
+n_head = 6 # 384 // 6 = 64 dimensional heads
+n_layer = 6
+dropout = 0.2 #20% get randomly shut off each loop
 #---------------
 
 torch.manual_seed(1337)
@@ -67,6 +70,8 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embed, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size,block_size)))
 
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x) # (B,T,C)
@@ -75,6 +80,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1) * C**-0.5 #(B,T,C) @ (B,C,T) -> (B,T,T)
         wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) #(B,T,T)
         wei = F.softmax(wei, dim=-1) #(B,T,T)
+        wei = self.dropout(wei)
         #perform weighted aggregation of values
         v = self.value(x) #(B,T,C)
         out = wei @ v #(B,T,T) @ (B,T,C) -> (B,T,C)
@@ -85,10 +91,11 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embed, n_embed)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.proj(out)
+        out = self.dropout(self.proj(out))
         return out
     
 class FeedForward(nn.Module):
@@ -98,6 +105,7 @@ class FeedForward(nn.Module):
             nn.Linear(n_embed, 4 * n_embed),
             nn.ReLU(),
             nn.Linear(4 * n_embed,n_embed),
+            nn.Dropout(dropout)
         )
     def forward(self, x):
         return self.net(x)
@@ -123,22 +131,19 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.blocks = nn.Sequential(
-            Block(n_embed,n_head=4),
-            Block(n_embed,n_head=4),
-            Block(n_embed,n_head=4),
-            nn.LayerNorm(n_embed),
-        )
+        self.blocks = nn.Sequential(*[Block(n_embed, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
         B,T = idx.shape
 
         tok_emb = self.token_embedding_table(idx) #(B,T,C)
-        pos_embed = self.position_embedding_table(torch.arange(T, device=device))
-        x = tok_emb + pos_embed
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+        x = tok_emb + pos_emb
         x = self.blocks(x)
-        logits = self.lm_head(x) 
+        x = self.ln_f(x)
+        logits = self.lm_head(x)
         
         if targets is None:
             loss = None
@@ -169,15 +174,17 @@ class BigramLanguageModel(nn.Module):
     
 model = BigramLanguageModel(vocab_size)
 m = model.to(device)
+# print the number of parameters in the model
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
 
 #create a PyTorch optimizer
-optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
 
 #Training loop
 for iter in range(max_iters):
 
     #every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0:
+    if iter % eval_interval == 0 or iter == max_iters - 1:
         losses= estimate_loss()
         print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
@@ -191,4 +198,4 @@ for iter in range(max_iters):
 
 #generate from the model
 context = torch.zeros((1,1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=2000)[0].tolist()))
